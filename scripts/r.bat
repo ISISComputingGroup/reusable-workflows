1361371eca7c66898c612ec943b046459527be32
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-10-07 14:18:26 +0100

    Check for 0 args

--- scripts/r.bat
+++ scripts/r.bat
@@ -1,4 +1,6 @@
 @echo off
+IF "%1"=="" GOTO HAVE_0_ARGS
+
 FOR /F "delims=" %%i IN ('git rev-parse --git-dir') DO set REUSABLE_WF_GITDIR=%%i
 if exist "%REUSABLE_WF_GITDIR%/../ruff.toml" (
   set "REUSABLE_WF_RUFF_CONFIG=%REUSABLE_WF_GITDIR%/../ruff.toml"
@@ -7,3 +9,6 @@ if exist "%REUSABLE_WF_GITDIR%/../ruff.toml" (
 )
 c:\instrument\apps\Python3\scripts\ruff %* --config="%REUSABLE_WF_RUFF_CONFIG%"
 exit /b %errorlevel%
+
+:HAVE_0_ARGS
+echo "Usage: either 'r check' or 'r format'"
